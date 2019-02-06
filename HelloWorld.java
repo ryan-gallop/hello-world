9a669498b99c7dd9ebaf7e173e4b86639220f12c
ryan-gallop <ryan.gallop@maine.edu>
2019-02-06 11:14:22 -0500

    #3 working on issue #3

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+    	System.out.println("Hear ye: Important announcement...");
+        System.out.println("Hello World");
+        System.out.println("GOODBYE!!!!");
+        System.out.println("Hi alice :)");
+        System.out.println("Hi bob ;*");
+        System.out.println("Eyyy charlie");
+        System.out.println("Hey there denise");
+     }
+}
